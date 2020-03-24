de1a12a03c3d39755c8b5d1daedca8eeb0918285
deanwiebe <wiebedean@gmail.com>
2020-03-24 15:16:39 -0600

    added hint.css and bootstrap for styling header complete. partial vacab finished

---
+++ index.php
@@ -0,0 +1,42 @@
+
+
+
+
+<?php include('templates/header.php'); ?>
+
+
+
+<main class="shadow p-3 mb-5  rounded">
+    <div class="wrapper">
+        <h2>List of Terms and Definitions</h2>
+        <ul>
+            <li><span class="hint--top badge badge-secondary" aria-label="Responsive Web Design">RWD</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="A unit of measurement used to
+            make text readable on mobile">Device Independant Pixel</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="A users visable
+            portion of a webpage. varying by device">Viewport</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="A JavaScript Library">Jquery</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="CSS tool for adapting to different screen 
+            sizes">Media Query</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="A popular CSS Javascript 
+            Library">Boostrap</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="A CSS tool used for flexable layout
+            ">Flex</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="A software Development website
+            used by developers to share and find code">GitHub</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="Scalable Vector Graphic">SVG</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="HTML5 used to create flexibility
+            around showing different pictures at different screen sizes">Picture Element</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="Thank you!">Tap target</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="Thank you!">Front-end Development</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="Thank you!">Front-end Developer</span></li>
+            <li><span class="hint--top badge badge-secondary" aria-label="Thank you!">Full-stack Developer</span></li>
+        </ul>
+    </div>
+</main>
+
+
+
+
+
+<?php include 'templates/footer.php';
--- templates/header.php
+++ templates/header.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/normalize.css">
+    <link rel="stylesheet" href="styles/hint.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Ass8</title>
 </head>
 <body>
-    
-</body>
-</html>
+    <header>
+        <p>Dean Wiebe</p>
+        <h1>Assignment 8 Use CSS Libraries</h1>
+        <p>000427752</p>
+    </header>
