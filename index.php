cb2c4d18e9b4fd7ea0ab9d3f39f282a9d1226089
deanwiebe <wiebedean@gmail.com>
2020-03-24 15:36:58 -0600

    added hint--medium to all li

--- index.php
+++ index.php
@@ -10,27 +10,27 @@
     <div class="wrapper">
         <h2>List of Terms and Definitions</h2>
         <ul>
-            <li><span class="hint--top badge badge-secondary" aria-label="Responsive Web Design">RWD</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="A unit of measurement used to
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="Responsive Web Design">RWD</span></li>
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="A unit of measurement used to
             make text readable on mobile">Device Independant Pixel</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="A users visable
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="A users visable
             portion of a webpage. varying by device">Viewport</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="A JavaScript Library">Jquery</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="CSS tool for adapting to different screen 
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="A JavaScript Library">Jquery</span></li>
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="CSS tool for adapting to different screen 
             sizes">Media Query</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="A popular CSS Javascript 
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="A popular CSS Javascript 
             Library">Boostrap</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="A CSS tool used for flexable layout
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="A CSS tool used for flexable layout
             ">Flex</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="A software Development website
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="A software Development website
             used by developers to share and find code">GitHub</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="Scalable Vector Graphic">SVG</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="HTML5 used to create flexibility
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="Scalable Vector Graphic">SVG</span></li>
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="HTML5 used to create flexibility
             around showing different pictures at different screen sizes">Picture Element</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="Thank you!">Tap target</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="Thank you!">Front-end Development</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="Thank you!">Front-end Developer</span></li>
-            <li><span class="hint--top badge badge-secondary" aria-label="Thank you!">Full-stack Developer</span></li>
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="Thank you!">Tap target</span></li>
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="Thank you!">Front-end Development</span></li>
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="Thank you!">Front-end Developer</span></li>
+            <li><span class="hint--top hint--medium badge badge-secondary" aria-label="Thank you!">Full-stack Developer</span></li>
         </ul>
     </div>
 </main>
